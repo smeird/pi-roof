63c5d68ab79e6107387a8890d472633e313ef136
smeird <dom@smeird.com>
2026-01-31 10:42:39 +0000

    Make roof commands fire-and-forget

--- scripts/roof/close.sh
+++ scripts/roof/close.sh
@@ -7,6 +7,5 @@ ROOF_BASE_URL="${ROOF_BASE_URL:-http://data.smeird.com:1880}"
 ROOF_HTTP_PATH="${ROOF_HTTP_PATH:-/api/roof}"
 CURL_TIMEOUT_SECS="${CURL_TIMEOUT_SECS:-20}"
 
-if ! curl -fsS --max-time "${CURL_TIMEOUT_SECS}" -H "Content-Type: application/json" -d '{"action":"close"}' "${ROOF_BASE_URL}${ROOF_HTTP_PATH}" > /dev/null; then
-  exit 1
-fi
+curl -fsS --max-time "${CURL_TIMEOUT_SECS}" -H "Content-Type: application/json" -d '{"action":"close"}' "${ROOF_BASE_URL}${ROOF_HTTP_PATH}" > /dev/null 2>&1 &
+disown
